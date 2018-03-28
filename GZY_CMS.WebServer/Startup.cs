dd5ac845d8a27bea4890c37334289aac936caf99
GuZhenYin <524808775@qq.com>
2018-03-28 15:40:22 +0800

    新增Web后台管理

--- GZY_CMS.WebServer/Startup.cs
+++ GZY_CMS.WebServer/Startup.cs
@@ -6,11 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using GZY_CMS.Core.Autofac;
-using GZY_CMS.Model;
-using Microsoft.EntityFrameworkCore;
 
-namespace GZY_CMS.WebClient
+namespace GZY_CMS.WebServer
 {
     public class Startup
     {
@@ -22,13 +19,9 @@ namespace GZY_CMS.WebClient
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.RegisterController();
             services.AddMvc();
-            services.AddDbContextPool<GZYCMSContext>(
-                    options => options.UseMySql(@"Server=111.231.81.169;database=GZY_CMS;uid=root;pwd=Gzy*123456;"));
-            return services.ReplacementIOC(new AutofacModule());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +34,7 @@ namespace GZY_CMS.WebClient
             }
             else
             {
-                app.UseExceptionHandler(" / Home/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
